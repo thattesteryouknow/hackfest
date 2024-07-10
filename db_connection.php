3d0ff2f295adb1991d69893907e604c87c302ade
CodeintheShadows <tanishkasaraswat6@gmail.com>
2024-07-11 01:24:10 +0530

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,20 @@
+<?php
+// db_connection.php
+
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "ecoparali";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die("Connect failed: %s\n". $conn -> error);
+
+    return $conn;
+}
+
+function CloseCon($conn)
+{
+    $conn -> close();
+}
+?>
